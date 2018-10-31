6ce7e50d883c76e47e78f5f80b6d374b57b18119
assadiabira <37332054+assadiabira@users.noreply.github.com>
2018-10-31 11:24:24 +0100

    Update consultation_mysql.php

--- consultation_mysql.php
+++ consultation_mysql.php
@@ -1,24 +1,27 @@
-<?php 
-// on se connecte à MySQL 
-$db = mysql_connect('localhost', 'login', 'password'); 
+#!/path/to/php
+<?ph
+$serveur= "mysql";
+$db = "usecase";
+$table = "famille_tbl";
+// on se connecte à MySQL
+$connect = new mysqli($serveur);
+// on seléctionne la base
+mysql_select_db($db);
 
-// on sélectionne la base 
-mysql_select_db('nom_de_la_base',$db); 
+// on crée la requete SQL
+$sql = "SELECT nom,prenom,statut,DATE_FORMAT(date, '%d-%m-%Y') as datefr FROM famille_tbl";
 
-// on crée la requête SQL 
-$sql = 'SELECT nom,prenom,statut,date FROM famille_tbl'; 
+// on envoie la requête
+$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 
-// on envoie la requête 
-$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error()); 
+// on fait une boucle qui va faire un tour pour chaque enregistrement
+while($data = mysql_fetch_array($req))
+    {
+    // on affiche les informations de l'enregistrement en cours
+    echo '<b>'.$data['nom'].' '.$data['prenom'].'</b> ('.$data['statut'].')';
+    echo ' <i>date de naissance : '.$data['datefr'].'</i><br>';
+    }
 
-// on fait une boucle qui va faire un tour pour chaque enregistrement 
-while($data = mysql_fetch_assoc($req)) 
-    { 
-    // on affiche les informations de l'enregistrement en cours 
-    echo '<b>'.$data['nom'].' '.$data['prenom'].'</b> ('.$data['statut'].')'; 
-    echo ' <i>date de naissance : '.$data['date'].'</i><br>'; 
-    } 
-
-// on ferme la connexion à mysql 
-mysql_close(); 
-?> 
+// on ferme la connexion à mysql
+mysql_close();
+?>
